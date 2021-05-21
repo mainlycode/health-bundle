2b6941af7c6bb6f39ba5b178e805c51b7d61e30b
othillo <othillo@othillo.nl>
2021-05-21 12:11:15 +0200

    initial commit

---
+++ src/HealthBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MainlyCode\HealthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HealthBundle extends Bundle
+{
+
+}
---
+++ tests/Controller/HealthControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MainlyCode\HealthBundle\Controller;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class HealthControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_a_200_ok()
+    {
+        $controller = new HealthController();
+
+        /** @var Response */
+        $response = $controller();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('👍', $response->getContent());
+    }
+}
